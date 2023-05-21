f3670603b8694971176cdf148d090d92f2d77f5e
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-05-21 18:41:01 +0600

    added update operation

--- index.js
+++ index.js
@@ -53,12 +53,27 @@ async function run() {
 
     app.delete('/toys/:id', async (req, res) => {
       const id = req.params.id;
+      console.log(id)
       const query = { _id: new ObjectId(id) }
       const result = await toyCollection.deleteOne(query)
       res.send(result)
     })
 
-
+    app.patch('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updatedBooking = req.body
+      console.log(updatedBooking)
+      const updateDoc = {
+        $set: {
+          price: updatedBooking.price,
+          available_quantity: updatedBooking.available_quantity,
+          description: updatedBooking.description
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
